472ceb6879c591c03f3d900ace907c5d9dc125a8
Guille <guillermodorfman@gmail.com>
2022-02-16 17:04:33 -0300

    more cleaning

--- Components/MainCont/MainCont.jsx
+++ Components/MainCont/MainCont.jsx
@@ -17,9 +17,6 @@ const MainCont = () => {
     },[from])
 
 
-    console.log(from, to);
-
-
   return (
 
       <>
--- Components/Topbar/Topbar.jsx
+++ Components/Topbar/Topbar.jsx
@@ -2,11 +2,13 @@ import React, {useContext} from 'react'
 import './Topbar.scss'
 import InfoIcon from '@mui/icons-material/Info';
 import { Link } from 'react-router-dom';
-
+import { useLocation } from 'react-router-dom'
 import { MovieListContext } from '../../context/MovieListContext';
 
 const Topbar = () => {
   const { setEndGame, setTimeLine, setShowMovieList } = useContext(MovieListContext)
+  
+  const location = useLocation();
   
 function resetGame(){
   setEndGame(false)
@@ -17,8 +19,10 @@ function resetGame(){
   return (
     <div className="topbar"> 
       <div className="buttons">
-        <button onClick={()=>resetGame()} className='left'><Link to={'/'}>BACK</Link></button>
-        <button className='right'><InfoIcon/>HOW TO PLAY</button>
+        { location.pathname === '/' ? <></> :
+          <button onClick={()=>resetGame()} className='left'><Link to={'/'}>BACK</Link></button>
+        }
+        <button className='right'><InfoIcon className="icon" /><b>HOW TO PLAY</b></button>
       </div>
         <div className="title-cont">
           <h1 className="title">Movie<b>2</b>Movie</h1>
